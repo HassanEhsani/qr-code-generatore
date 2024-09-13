aa30549ed9fc847843bd4e2e8d7fa9f8d8bd961b
HassanEhsani <hassan.ehsani.am@gmail.com>
2024-09-13 20:38:58 +0300

    and also we can ask from the user input your website for generate qrcode

--- python_app.py
+++ python_app.py
@@ -2,4 +2,22 @@
 # create a funtion that collects a text and converts it to a qrcode
 # save the qrcode as an image
 # run the function
+import qrcode
 
+def generate_qrcode(text):
+    
+    qr = qrcode.QRCode(
+        version = 1,
+        error_correction = qrcode.constants.ERROR_CORRECT_L,
+        box_size = 20,
+        border = 8,
+    )
+    
+    qr.add_data(text)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save("qrimg.png")
+    
+    
+url = input("Enter your url: ")    
+generate_qrcode("https://www.google.com")
